b812784608895af96d4464b8c6ca2f096e7fd3bb
Ashish <73551845+Ashish2468@users.noreply.github.com>
2022-02-11 19:01:21 +0530

    Hosting

--- App.js
+++ App.js
@@ -1,48 +1,4 @@
-// /**
-//  * Sample React Native App
-//  * https://github.com/facebook/react-native
-//  *
-//  * @format
-//  * @flow strict-local
-//  */
-
-// import React from 'react';
-// import {
-//   SafeAreaView,
-//   ScrollView,
-//   StatusBar,
-//   StyleSheet,
-//   Text,
-//   useColorScheme,
-//   View,
-// } from 'react-native';
-
-// import {
-//   Colors,
-//   DebugInstructions,
-//   Header,
-//   LearnMoreLinks,
-//   ReloadInstructions,
-// } from 'react-native/Libraries/NewAppScreen';
-
-// const App = () => {
-//   return (
-//     <SafeAreaView>
-//       <Text>Hi uiejrfre</Text>
-//       <Text>Hi uiejrfre</Text>
-//       <Text>Hi uiejrfre</Text>
-//       <Text>Hi uiejrfre</Text>
-//       <Text>Hi uiejrfre</Text>
-//       <Text>Hi uiejrfre</Text>
-//       <Text>Hi uiejrfre</Text>
-//       <Text>Hi uiejrfre</Text>
-//     </SafeAreaView>
-//   );
-// };
-
-// const styles = StyleSheet.create({});
-
-// export default App;
+export default App;
 
 import React from 'react';
 import AppContainer from './src/navigation/AppContainer';
